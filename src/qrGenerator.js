87807b5c17918bd843fc29fa8e6ee28b07957bd3
Batunash <okullubatuhan@gmail.com>
2025-06-21 15:27:00 +0300

    bug fix

--- src/qrGenerator.js
+++ src/qrGenerator.js
@@ -1,27 +1,12 @@
 const QRCode = require('qrcode');
 
-/**
- * Generate QR code data URL from a link
- * @param {string} link - The link to encode
- * @returns {Promise<string>} QR code data URL
- */
-async function generateQRCode(link) {
-  const options = {
-    type: 'image/png',
-    width: 256,
-    margin: 1,
-    color: {
-      dark: '#000000',
-      light: '#FFFFFF'
-    }
-  };
-  
+async function generateQRCode(link, options={}) {
   try {
-    const dataURL = await QRCode.toDataURL(link, options);
+    const dataURL = await QRCode.toDataURL(link);
+    console.log(dataURL)
     return dataURL;
   } catch (err) {
     throw new Error(`Failed to generate QR code: ${err.message}`);
   }
 }
-
 module.exports = { generateQRCode };
